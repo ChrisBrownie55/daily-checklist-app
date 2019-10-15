20d7508df1fc06c007bad0d1eccd0bddb9e85ef0
Christopher Brown <chbphone55@gmail.com>
2019-10-14 21:23:22 -0500

    ya yee

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+// colors: https://material.io/resources/color/#!/?view.left=0&view.right=0&secondary.color=1675d1&primary.color=512DA8
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import useAsyncStorageJSON from './lib/use-async-storage-json';
+
+function Task() {
+  return null;
+}
 
 export default function App() {
+  const [tasks, setTasks, clearTasks] = useAsyncStorageJSON('tasks', []);
+
+  console.log('Tasks', tasks);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {tasks.map(data => (
+        <Task />
+      ))}
     </View>
   );
 }

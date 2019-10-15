3cfd95fcf9ed63557d279e7b6a8e83f975155fe9
Christopher Brown <chbphone55@gmail.com>
2019-10-15 18:47:18 -0500

    kinda forming up

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 // colors: https://material.io/resources/color/#!/?view.left=0&view.right=0&secondary.color=1675d1&primary.color=512DA8
 
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableNativeFeedback,
+  Button,
+  Switch
+} from 'react-native';
 import FAB from 'react-native-fab';
 import { MaterialIcons } from '@expo/vector-icons';
 
@@ -9,24 +16,85 @@ import useAsyncStorageJSON from './lib/use-async-storage-json';
 import CheckBox from './components/CheckBox';
 
 function Task({ title, timeLimit, done, handleUpdate }) {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <View style={Task.styles.container}>
-      <CheckBox checked={done} />
-      <Text>{title}</Text>
+    <View style={Task.styles.outerContainer}>
+      <TouchableNativeFeedback onPress={() => setOpen(!isOpen)}>
+        <View style={Task.styles.innerContainer}>
+          <CheckBox style={Task.styles.checkbox} checked={done} />
+          <Text style={Task.styles.title}>{title}</Text>
+          {timeLimit.active && (
+            <Text style={Task.styles.timeLimit}>{timeLimit.minutes} min</Text>
+          )}
+        </View>
+      </TouchableNativeFeedback>
+      {isOpen && (
+        <View style={Task.styles.editContainer}>
+          <View style={Task.styles.titleSection}>
+            <Text>Title</Text>
+          </View>
+          <View style={Task.styles.timeLimitSection}>
+            <Text>Time limit</Text>
+            <Switch value={timeLimit.active} onValueChange={() => {}} />
+          </View>
+          <View style={Task.styles.actions}>
+            <Button title="cancel">CANCEL</Button>
+            <Button title="done">DONE</Button>
+          </View>
+        </View>
+      )}
     </View>
   );
 }
 
 Task.styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 20,
+  outerContainer: {
+    alignSelf: 'stretch',
+    backgroundColor: 'white',
+
     borderColor: '#ADD1F5',
     borderWidth: 1,
     borderStyle: 'solid',
     borderRadius: 8,
-    alignSelf: 'stretch'
+
+    shadowOpacity: 0.05,
+    shadowRadius: 1,
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowColor: '#125596',
+    elevation: 1,
+
+    overflow: 'hidden'
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+
+    paddingVertical: 20,
+    paddingHorizontal: 25
+  },
+  checkbox: {
+    marginRight: 10
+  },
+  title: {},
+  timeLimit: {
+    marginLeft: 'auto'
+  },
+
+  editContainer: {},
+  titleSection: {
+    flexDirection: 'row'
+  },
+  timeLimitSection: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
   }
 });
 
@@ -77,10 +145,8 @@ App.styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 45,
-    paddingBottom: 45,
-    paddingLeft: 30,
-    paddingRight: 30
+    paddingVertical: 45,
+    paddingHorizontal: 30
   },
   title: {
     fontSize: 32,

11417a3436c33abff48702a46c6f25d88bbb145a
Christopher Brown <chbphone55@gmail.com>
2019-10-16 17:03:34 -0500

    Swipeable

--- App.js
+++ App.js
@@ -1,56 +1,147 @@
 // colors: https://material.io/resources/color/#!/?view.left=0&view.right=0&secondary.color=1675d1&primary.color=512DA8
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   Text,
   View,
   TouchableNativeFeedback,
   Button,
-  Switch
+  Switch,
+  TextInput
 } from 'react-native';
 import FAB from 'react-native-fab';
 import { MaterialIcons } from '@expo/vector-icons';
 
 import useAsyncStorageJSON from './lib/use-async-storage-json';
 import CheckBox from './components/CheckBox';
+import Swipeable from 'react-native-swipeable';
 
-function Task({ title, timeLimit, done, handleUpdate }) {
-  const [isOpen, setOpen] = useState(false);
+function useStateWithDefault(defaultValue) {
+  const [value, setValue] = useState(defaultValue);
+
+  useEffect(() => {
+    if (value !== defaultValue) {
+      setValue(defaultValue);
+    }
+  }, [defaultValue]);
+
+  function reset() {
+    setValue(defaultValue);
+  }
+
+  return [value, setValue, reset];
+}
+
+function Task({ title, timeLimit, done, handleUpdate, isOpen, onToggleOpen }) {
+  const [newTitle, setNewTitle, resetNewTitle] = useStateWithDefault(title);
+  const [
+    isTimeLimitActive,
+    setTimeLimitActive,
+    resetTimeLimitActive
+  ] = useStateWithDefault(timeLimit.active);
+  const [
+    timeLimitMinutes,
+    setTimeLimitMinutes,
+    resetTimeLimitMinutes
+  ] = useStateWithDefault(timeLimit.minutes);
+  function incrementTimeLimitMinutes() {
+    setTimeLimitMinutes(timeLimitMinutes + 1);
+  }
+  function decrementTimeLimitMinutes() {
+    setTimeLimitMinutes(Math.max(0, timeLimitMinutes - 1));
+  }
+
+  function cancel() {
+    resetNewTitle();
+    resetTimeLimitActive();
+    resetTimeLimitMinutes();
+
+    if (isOpen) {
+      onToggleOpen();
+    }
+  }
+
+  function submitChange() {
+    handleUpdate({
+      title: newTitle,
+      timeLimit: {
+        active: isTimeLimitActive,
+        minutes: timeLimitMinutes
+      },
+      done
+    });
+  }
+
+  function toggleDone() {
+    handleUpdate({
+      title,
+      timeLimit,
+      done: !done
+    });
+  }
+
+  const [isDeleting, setDeleting] = useState(false);
 
   return (
-    <View style={Task.styles.outerContainer}>
-      <TouchableNativeFeedback onPress={() => setOpen(!isOpen)}>
-        <View style={Task.styles.innerContainer}>
-          <CheckBox style={Task.styles.checkbox} checked={done} />
-          <Text style={Task.styles.title}>{title}</Text>
-          {timeLimit.active && (
-            <Text style={Task.styles.timeLimit}>{timeLimit.minutes} min</Text>
-          )}
+    <Swipeable
+      rightContent={
+        <View style={{ flex: 1, justifyContent: 'center', paddingLeft: 20 }}>
+          <Text style={{ color: isDeleting ? '#1976D2' : 'lightgrey' }}>
+            Remove item
+          </Text>
         </View>
-      </TouchableNativeFeedback>
-      {isOpen && (
-        <View style={Task.styles.editContainer}>
-          <View style={Task.styles.titleSection}>
-            <Text>Title</Text>
+      }
+      style={{ overflow: 'hidden' }}
+      onRightActionActivate={() => setDeleting(true)}
+      onRightActionDeactivate={() => setDeleting(false)}
+      onRightActionRelease={() => console.log('Removing item')}
+    >
+      <View style={Task.styles.outerContainer}>
+        <TouchableNativeFeedback onPress={onToggleOpen}>
+          <View style={Task.styles.innerContainer}>
+            <CheckBox
+              style={Task.styles.checkbox}
+              checked={done}
+              onPress={toggleDone}
+            />
+            <Text style={Task.styles.title}>{title}</Text>
+            {timeLimit.active && (
+              <Text style={Task.styles.timeLimit}>{timeLimit.minutes} min</Text>
+            )}
           </View>
-          <View style={Task.styles.timeLimitSection}>
-            <Text>Time limit</Text>
-            <Switch value={timeLimit.active} onValueChange={() => {}} />
+        </TouchableNativeFeedback>
+        {isOpen && (
+          <View style={Task.styles.editContainer}>
+            <View style={Task.styles.titleSection}>
+              <Text>Title</Text>
+              <TextInput onChangeText={setNewTitle} value={newTitle} />
+            </View>
+            <View style={Task.styles.timeLimitSection}>
+              <Text>Time limit</Text>
+              <Switch
+                value={isTimeLimitActive}
+                onValueChange={() => setTimeLimitActive(!isTimeLimitActive)}
+              />
+              <View></View>
+            </View>
+            <View style={Task.styles.actions}>
+              <Button title="cancel" onPress={cancel}>
+                CANCEL
+              </Button>
+              <Button title="done">DONE</Button>
+            </View>
           </View>
-          <View style={Task.styles.actions}>
-            <Button title="cancel">CANCEL</Button>
-            <Button title="done">DONE</Button>
-          </View>
-        </View>
-      )}
-    </View>
+        )}
+      </View>
+    </Swipeable>
   );
 }
 
 Task.styles = StyleSheet.create({
   outerContainer: {
     alignSelf: 'stretch',
+    marginBottom: 10,
     backgroundColor: 'white',
 
     borderColor: '#ADD1F5',
@@ -84,7 +175,10 @@ Task.styles = StyleSheet.create({
     marginLeft: 'auto'
   },
 
-  editContainer: {},
+  editContainer: {
+    paddingBottom: 20,
+    paddingHorizontal: 30
+  },
   titleSection: {
     flexDirection: 'row'
   },
@@ -100,6 +194,7 @@ Task.styles = StyleSheet.create({
 
 export default function App() {
   const [tasks, setTasks, clearTasks] = useAsyncStorageJSON('tasks', []);
+  const [openTaskIndex, setOpenTaskIndex] = useState(null);
 
   function newTask() {
     setTasks([
@@ -126,6 +221,10 @@ export default function App() {
           <Task
             key={index}
             handleUpdate={task => editTask(task, index)}
+            isOpen={openTaskIndex === index}
+            onToggleOpen={() =>
+              setOpenTaskIndex(openTaskIndex === index ? null : index)
+            }
             {...task}
           />
         ))}
